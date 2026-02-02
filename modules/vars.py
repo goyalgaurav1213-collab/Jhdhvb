4147b970698414991a15aa14cdc93969af70df60
goyalgaurav1213-collab <goyalgaurav1213@gmail.com>
2026-02-02 17:17:17 +0530

    Set default API_ID and API_HASH
    Update default API_ID and API_HASH values in vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,7 +1,7 @@
 import os
 
-API_ID    = os.environ.get("API_ID", "")
-API_HASH  = os.environ.get("API_HASH", "")
+API_ID    = os.environ.get("API_ID", "23035668")
+API_HASH  = os.environ.get("API_HASH", "37e73a238b2258a1294cdbd63e4e934f")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 
 WEBHOOK = True  # Don't change this
